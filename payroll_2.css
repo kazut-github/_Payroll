026d3b5d3956919ac4aeabb2f8d1b0bcf4114bf6
kazut-github <69755438+kazut-github@users.noreply.github.com>
2025-05-01 23:21:47 +0900

    Create payroll_2.css

---
+++ payroll_2.css
@@ -0,0 +1,116 @@
+body {
+    margin: 0 auto;
+}
+header {
+    background-color: rgba(0, 0, 255, 0.981);
+    color: white;
+    font-size: large;
+    font-family: sans-serif;
+    /* font-weight: bolder;  */
+    /* opacity: 0.4; */
+    width: 100%;
+    box-sizing: border-box;
+    /* margin: 0 auto; */
+    position: fixed;
+    /* top: 10px; */
+    z-index: 10; 
+}
+.col-6 {
+    text-align: center;
+    font-weight: bolder;
+    color: coral;
+    cursor: pointer;
+}
+.col-3 {
+    cursor: pointer;
+}
+.top-wrapper h1{
+    margin: 3.5rem 0 0 2rem; 
+    margin-bottom: 30px;
+    font-family: sans-serif;
+    font-weight: bolder;
+}
+.one_salary::-webkit-inner-spin-button,
+.one_salary::-webkit-outer-spin-button{
+    -webkit-appearance: none;
+    margin: 0;
+    -moz-appearance:textfield;
+}
+.hr {
+    border-top: 20px double #000;
+  }
+
+/* ミドルラッパー */
+.main-container  {
+     
+}
+/* ミドルラッパー */
+.middle-wrappar input {
+    /* 影をつける */
+    /* box-shadow: 1px 1px 1px 1px rgba(0,0,0,0.1) inset; */
+    /* color: blue; */
+}
+#js-daySalary, #js-total, #js-daySalary-sec, #js-total-sec{
+    color: #0000ff;
+    font-weight: bold;
+    /* text-decoration:underline; */
+    text-decoration-color:#FF0000;
+
+}
+::placeholder {
+    color: red;
+    font-weight: bold;
+    text-decoration: none;
+}
+/* .txt3{
+    display: inline-block;
+    /*コレ*/
+    /* width: 10em; */
+    /*コレ*/
+    /* max-width: 100%;
+    padding: 0.5em;
+    border: 1px solid #999;
+    box-sizing: border-box;
+    background: #f2f2f2;
+    margin: 0.5em 0;
+} */
+/* <input type="text" class="txt3" value="テキスト"> */
+
+.calculator {
+    /* float: right; */
+    float: left;
+    margin-right: 2rem;
+    margin-top: 1.5rem;
+    /* padding: 10px; */
+}
+.calculate_btn {
+    color: white;
+    background-color: blue;
+    /* ↓pc，タブレット対応時画面適応 */
+    /* padding: 4px 15px; */
+ }
+.reset_btn {
+    color: black;
+    background-color: rgb(238, 238, 150);
+    font-weight: bolder;
+    /* ↓pc，タブレット対応時画面適応 */
+    /* padding: 4px 10px; */
+} 
+.save-btn {
+    color: rgb(248, 244, 244);
+    background-color: rgb(21, 128, 21);
+    /* float: right; */
+    /* margin-right: 5px; */
+
+}
+
+/* フッター */
+footer {
+    background-color: yellowgreen;
+}
+footer p {
+    margin-top: 3rem;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bolder;
+}
